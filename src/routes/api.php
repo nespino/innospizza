87d112b0441b40490a952cac97fdcc30e69a34a7
Nahuel Espiño <estudio@nahuelespino.com.ar>
2020-07-30 04:00:50 -0300

    Removing unuseful code

--- src/routes/api.php
+++ src/routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::get('/products', 'ProductController@index')->name('products.all');
 
 Route::post('/products', 'ProductController@store')->name('products.store');
